a1ca9e5d7061e8baa4c088020080ae985ee5a9a1
Morten Lindhardt <r3Fuze@gmail.com>
2022-04-21 23:44:18 +0200

    fix: reduce the amount of lorem ipsum on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,23 +5,16 @@ const About: NextPage = () => {
     <div>
       <h1>About Page</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-        cupiditate sunt animi obcaecati dicta hic eum nam minima. Nulla ea
-        ducimus dolorem, voluptatem laboriosam mollitia aut nobis aspernatur
-        quod possimus. Iste voluptas quaerat reiciendis rem ipsum exercitationem
-        impedit, dignissimos corrupti voluptatibus ducimus debitis rerum odio
-        placeat porro vero consectetur omnis labore reprehenderit? Reiciendis
-        dolor ipsa at cupiditate, quidem quibusdam nihil. Incidunt veritatis
-        laborum eaque quibusdam corporis excepturi, blanditiis minus voluptate
-        esse sit ea nobis voluptatibus neque magni quaerat nisi nulla placeat?
-        Qui alias molestiae accusantium doloremque sed aut laborum reiciendis!
-        Fugit laborum odio minus architecto soluta culpa ut nisi porro velit
-        optio, reiciendis hic non quaerat recusandae ab! Iste natus est
-        consequatur exercitationem nostrum temporibus dolores at rerum porro
-        nisi! Labore reiciendis adipisci velit laudantium debitis rerum,
-        eligendi at minima, temporibus, recusandae molestias nostrum cum porro
-        possimus optio ullam fugiat quis doloribus. Dicta enim voluptatem velit
-        aut ea ipsam eius.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quas
+        nesciunt quaerat cupiditate illo vero a nostrum minima molestias,
+        dolorem sapiente expedita neque dolorum veritatis? Temporibus eligendi
+        id iusto enim? Dolor, soluta! Corrupti explicabo rem ut porro, sunt
+        quam. Quos, perferendis? Corrupti temporibus quibusdam at hic quia
+        laborum iste itaque odio ipsam amet animi, corporis quisquam culpa sint,
+        aliquam tempore? Molestiae sunt distinctio maiores, optio quibusdam quo,
+        quisquam aut amet dolorem fugit, quos eius veniam officia tempora
+        numquam! Nam deserunt vero consectetur, deleniti perferendis praesentium
+        et tenetur modi sapiente quod.
       </p>
     </div>
   )
