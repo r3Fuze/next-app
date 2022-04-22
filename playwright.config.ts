30cb716f216067e6bb9b8bbc49b4b4af6199f9bb
Morten Lindhardt <r3fuze@gmail.com>
2022-04-22 21:40:02 +0200

    chore: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import type { PlaywrightTestConfig } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
   testDir: "./e2e",
+  reporter: process.env.CI ? "github" : "list",
 }
 
 export default config
