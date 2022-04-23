6f70d8fbfe968df49fb06f617dae768c4579af15
Morten Lindhardt <r3fuze@gmail.com>
2022-04-23 11:38:26 +0200

    feat: run tests, eslint, prettier, and commitlint when commiting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Card from "@/components/Card"
 import type { NextPage } from "next"
 import Image from "next/image"
-import Link from "next/link"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
